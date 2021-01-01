2329d2fe3d44be8c010c26d0f9514dadfb7d2f20
Christopher Conroy <39525900+cbc02009@users.noreply.github.com>
2021-01-01 18:55:21 -0500

    Fix math, use system random number generator.

--- gen_char.py
+++ gen_char.py
@@ -27,6 +27,7 @@ name_list = [
 
 if __name__ == '__main__':
 
-    i = random.randint(0, len(name_list))
+    sysrand = random.SystemRandom()
+    i = sysrand.randint(0, (len(name_list) - 1))
 
-    print(f"You are The {name_list[i]} Pirate. Please print pages {2 * i} - {2 * i  + 1} from playersheets.pdf")
+    print(f"You are The {name_list[i]} Pirate. Please print pages {2 * i + 1} - {2 * i  + 2} from playersheets.pdf")
