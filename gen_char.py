4aa527447708be91a53799a22b3577ad6598a2d5
Christopher Conroy <39525900+cbc02009@users.noreply.github.com>
2021-03-25 08:05:02 -0400

    use name list, other improvements.

--- gen_char.py
+++ gen_char.py
@@ -2,6 +2,9 @@
 import random
 
 name_list = [
+]
+
+job_list = [
     'Seeker',
     'All-Seeing',
     'Grifter',
@@ -28,6 +31,36 @@ name_list = [
 if __name__ == '__main__':
 
     sysrand = random.SystemRandom()
-    i = sysrand.randint(0, (len(name_list) - 1))
 
-    print(f"You are The {name_list[i]} Pirate. Please print pages {2 * i + 1} - {2 * i  + 2} from playersheets.pdf")
+    result_list = []
+    rand_list = []
+
+    num_of_players = len(name_list)
+
+    for index in range(num_of_players):
+
+        #Re-roll if already in list:
+        while True:
+            # i = random.randint(0, 20)
+            rand = sysrand.randint(0, 20)
+
+            print(f"Random: {rand}")
+
+            if rand not in rand_list:
+                result_list.append({
+                    'index': index,
+                    'name': name_list[index],
+                    'job': job_list[rand],
+                    'p1': 2 * rand + 1,
+                    'p2': 2 * rand + 2
+                })
+
+                rand_list.append(rand)
+
+                break
+
+            else:
+                print("Rerolling")
+
+    for entry in result_list:
+        print(f"{entry['name']}, you are The {entry['job']} Pirate. Please print pages {entry['p1']} - {entry['p2']} from playersheets.pdf")
